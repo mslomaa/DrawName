680c2e79f3c92e2831c00c0c377b324e3d8d34f2
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-30 23:44:09 +0200

    work in JS

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('button');
+
+const names = ['tomek', 'agnieszka', 'ola', 'zenek', 'ewa', 'pawel', 'asia']
+
+
+const nameGenerator = () => {
+
+}
+
+btn.addEventListener('click', nameGenerator)
