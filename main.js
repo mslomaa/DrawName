a8f66bb1f4292c8ea40d856043f2b7af80066aff
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-30 23:44:16 +0200

    work in JS

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const btn = document.querySelector('button');
 
+const div = document.querySelector('div');
+
 const names = ['tomek', 'agnieszka', 'ola', 'zenek', 'ewa', 'pawel', 'asia']
 
 
-const nameGenerator = () => {
 
+const nameGenerator = () => {
+    const index = Math.floor(Math.random() * names.length);
+    div.textContent = `wylosowane imie to ${name[index]}`;
 }
 
 btn.addEventListener('click', nameGenerator)
